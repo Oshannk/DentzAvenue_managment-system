d2370ca7171ed783b4bb8781eadeca76c32e1bd2
IranUdesha <49976786+IranUdesha@users.noreply.github.com>
2020-05-31 22:22:34 +0530

    Test Commit 001 Iran

--- Dentz_Avenue/src/dentz_avenue/Dentz_Avenue.java
+++ Dentz_Avenue/src/dentz_avenue/Dentz_Avenue.java
@@ -5,6 +5,8 @@
  */
 package dentz_avenue;
 
+import java.awt.BorderLayout;
+
 /**
  *
  * @author SHA
@@ -16,6 +18,9 @@ public class Dentz_Avenue {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Test 001 Iran");
+        
     }
     
 }
